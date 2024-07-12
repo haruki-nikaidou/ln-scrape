873900700818d5fdb5552a9aabe88ac32b1d427d
haruki <meteo.haru@gmail.com>
2024-07-13 06:08:05 +0800

    read secret map of bili novel

--- src/codebook/mod.rs
+++ src/codebook/mod.rs
@@ -1,3 +1,5 @@
+mod bili_secret_map;
+
 use std::collections::HashMap;
 
 pub struct SecretMap (HashMap<char,char>);
@@ -5,5 +7,16 @@ pub struct SecretMap (HashMap<char,char>);
 impl SecretMap {
     fn try_get(&self, key: char) -> Option<char> {
         self.0.get(&key).copied()
+    }
+
+    fn try_from_json(json_str: &str) -> Result<Self, serde_json::Error> {
+        let map: HashMap<char,char> = serde_json::from_str(json_str)?;
+        Ok(Self(map))
+    }
+
+    fn combine(a: Self, b: Self) -> Self {
+        let mut map = a.0;
+        map.extend(b.0);
+        Self(map)
     }
 }
